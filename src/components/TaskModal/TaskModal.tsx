fa48522538e72f65970cdee2eb41dc7693b9127d
Juan Vasquez <juancamilovasquezj@gmail.com>
2024-01-28 13:49:14 +0000

    refactor: modularise modals

--- src/components/TaskModal/TaskModal.tsx
+++ src/components/TaskModal/TaskModal.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useCreateTask, useUpdateTask } from '../../lib/mutations';
-import styles from './TaskModal.module.css';
 import { NexusGenObjects } from '../../graphql-server/generated/types';
-import Button from '../ui/Button/Button';
+import Modal from '../ui/Modal/Modal';
+import TaskForm from '../TaskForm/TaskForm';
 
 interface TaskModalProps {
   setIsModalActive: React.Dispatch<React.SetStateAction<boolean>>;
@@ -13,9 +13,8 @@ interface TaskModalProps {
 const TaskModal: React.FC<TaskModalProps> = (props) => {
   const { taskAction: addTask } = useCreateTask();
   const { taskAction: editTask } = useUpdateTask();
-  const [taskDetails, setTasksDetails] = useState(props.task);
-  console.log(taskDetails);
-
+  const [taskDetails, setTaskDetails] = useState(props.task);
+  console.log({ taskDetails });
   const closeModal = useCallback(() => props.setIsModalActive(false), [props]);
 
   useEffect(() => {
@@ -26,12 +25,10 @@ const TaskModal: React.FC<TaskModalProps> = (props) => {
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [closeModal]);
 
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setTasksDetails((prevState) => ({
+  const handleChange = (name: string, value: string) => {
+    setTaskDetails((prevState) => ({
       ...prevState,
-      [event.target.name]: event.target.value,
+      [name]: value,
     }));
   };
 
@@ -39,43 +36,16 @@ const TaskModal: React.FC<TaskModalProps> = (props) => {
     closeModal();
   };
 
+  const actionName = taskDetails ? 'Edit task' : 'Add task';
   return (
-    <div className={styles.overlay}>
-      <div className={styles.modal}>
-        <p className={styles.modal__close} onClick={closeModal}>
-          X
-        </p>
-        <h2>Add Task</h2>
-        <form className={styles.form}>
-          <div className={styles.form__item}>
-            <label htmlFor='text' className={styles.form__label}>
-              Title
-            </label>
-            <input
-              type='text'
-              name='title'
-              id='title'
-              className={styles.form__input}
-              onChange={handleChange}
-            />
-          </div>
-          <div className={styles.form__item}>
-            <label htmlFor='description' className={styles.form__label}>
-              Description
-            </label>
-            <textarea
-              name='description'
-              id='description'
-              cols={20}
-              rows={5}
-              className={styles.form__input}
-              onChange={handleChange}
-            ></textarea>
-          </div>
-          <Button text='Add Task' onClick={handleSubmit} />
-        </form>
-      </div>
-    </div>
+    <Modal onClose={closeModal} title={actionName}>
+      <TaskForm
+        task={taskDetails}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+        actionName={actionName}
+      />
+    </Modal>
   );
 };
 
